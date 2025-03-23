5b383c80acd8477acd770f244b98b74a27da1dc9
Vanh41 <vietanhm6a@gmail.com>
2025-03-23 10:30:06 +0700

    finish 9 and 10

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -5,5 +5,48 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
 
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
