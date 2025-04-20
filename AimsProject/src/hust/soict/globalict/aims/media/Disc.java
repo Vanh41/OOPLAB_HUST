8267899fbd42029b9648c0e105e4d1adc8a8dcb2
Vanh41 <vietanhm6a@gmail.com>
2025-04-20 22:38:20 +0700

    finish 10

---
+++ AimsProject/src/hust/soict/globalict/aims/media/Disc.java
@@ -0,0 +1,14 @@
+package hust.soict.globalict.aims.media;
+
+public class CompactDisc extends Media {
+    private int length;
+    private String director;
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+}
